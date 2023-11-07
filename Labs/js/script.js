34de835f732a9208fdc093af559f52bfc1b3c566
Volirvag <solidbush@mail.ru>
2023-11-07 12:37:34 +0300

    Fixed: tables and script

--- Labs/js/script.js
+++ Labs/js/script.js
@@ -7,19 +7,13 @@ window.onload = function () {
                 `${entry.name}: DOMContentLoaded processing time: ${domContentLoadedTime}ms`,
             );
             const statel = document.getElementById("load__stat")
-            if (statel){
-                statel.innerText = `DOMContentLoaded processing time: ${domContentLoadedTime}ms`
+            if (statel) {
+                statel.innerText = `DOMContentLoaded processing time: ${domContentLoadedTime}s`
             }
         });
     });
-    observer.observe({ type: "navigation", buffered: true });
+    observer.observe({type: "navigation", buffered: true});
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('login__button').onclick = function() {
-        window.location.href = 'http://localhost:63342/WebProject/LoginPage/index.html?_ijt=79nr3orggjanq9fd6og2h8jd7j';
-    };
-})
 
 
 
--- LoginPage/js/script.js
+++ LoginPage/js/script.js
@@ -1,17 +1,6 @@
-window.onload = function () {
-    const observer = new PerformanceObserver((list) => {
-        list.getEntries().forEach((entry) => {
-            const domContentLoadedTime =
-                entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart;
-            console.log(
-                `${entry.name}: DOMContentLoaded processing time: ${domContentLoadedTime}ms`,
-            );
-            const statel = document.getElementById("load__stat")
-            if (statel){
-                statel.innerText = `DOMContentLoaded processing time: ${domContentLoadedTime}ms`
-            }
-        });
-    });
-    observer.observe({ type: "navigation", buffered: true });
-}
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('login__button').onclick = function() {
+        window.location.href = 'http://localhost:63342/WebProject/LoginPage/index.html?_ijt=79nr3orggjanq9fd6og2h8jd7j';
+    };
+})
 
