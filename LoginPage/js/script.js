65b8c61b44012ee39c800b2dab81c1cc4659cf2d
Volirvag <solidbush@mail.ru>
2023-11-07 11:31:52 +0300

    Implemented: Scripts, table.

---
+++ LoginPage/js/script.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    const observer = new PerformanceObserver((list) => {
+        list.getEntries().forEach((entry) => {
+            const domContentLoadedTime =
+                entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart;
+            console.log(
+                `${entry.name}: DOMContentLoaded processing time: ${domContentLoadedTime}ms`,
+            );
+            const statel = document.getElementById("load__stat")
+            if (statel){
+                statel.innerText = `DOMContentLoaded processing time: ${domContentLoadedTime}ms`
+            }
+        });
+    });
+    observer.observe({ type: "navigation", buffered: true });
+}
+
