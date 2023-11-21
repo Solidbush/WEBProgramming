01a9d6e0394f3bf5ca6f4ab3d2dfc5decffa4e21
Volirvag <solidbush@mail.ru>
2023-11-21 12:21:12 +0300

    Fixed: Implemented localStorage and table in runtime. Made courses page

--- Labs/js/script.js
+++ Labs/js/script.js
@@ -2,13 +2,13 @@ window.onload = function () {
     const observer = new PerformanceObserver((list) => {
         list.getEntries().forEach((entry) => {
             const domContentLoadedTime =
-                entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart;
+                entry.loadEventEnd - entry.fetchStart;
             console.log(
                 `${entry.name}: DOMContentLoaded processing time: ${domContentLoadedTime}ms`,
             );
             const statel = document.getElementById("load__stat")
             if (statel) {
-                statel.innerText = `DOMContentLoaded processing time: ${domContentLoadedTime}s`
+                statel.innerText = `Load time: ${domContentLoadedTime}ms`
             }
         });
     });
--- LoginPage/js/script.js
+++ LoginPage/js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('login__button').onclick = function() {
-        window.location.href = 'http://localhost:63342/WebProject/LoginPage/index.html?_ijt=79nr3orggjanq9fd6og2h8jd7j';
+        window.location.href = "../LoginPage/index.html";
     };
 })
-
